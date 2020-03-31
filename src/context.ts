66f4d99dc9e0ffd9e980e6a8e5646986ed236ca3
vinaypuppal <me@vinaypuppal.com>
2020-03-31 13:54:18 +0530

    fix parsing of multiple query params with same name

--- src/context.ts
+++ src/context.ts
@@ -130,8 +130,15 @@ export class Context extends RouterResponse {
   }
   private _instanceToJson(instance: any) {
     return [...instance].reduce((obj, item) => {
-      const prop: { [key: string]: any } = {};
       const key = item[0] as string;
+      if (obj[key]) {
+        obj[key] = Array.isArray(obj[key])
+          ? [...obj[key], item[1]]
+          : [obj[key], item[1]];
+        return obj;
+      }
+
+      const prop: { [key: string]: any } = {};
       prop[key] = item[1];
       return { ...obj, ...prop };
     }, {});
