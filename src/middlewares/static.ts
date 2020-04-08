5f10499122c2b154ce10e83eabd0a113f8136303
vinaypuppal <me@vinaypuppal.com>
2020-04-08 15:56:07 +0530

    fix workersites types

--- src/middlewares/static.ts
+++ src/middlewares/static.ts
@@ -3,7 +3,7 @@ import { Options } from "@cloudflare/kv-asset-handler/src/types";
 
 import { Context } from "../context";
 
-export function workersSite(options: Options) {
+export function workersSite(options?: Partial<Options>) {
   return async (ctx: Context) => {
     try {
       const response = await getAssetFromKV(ctx.event, options);
