1321539c2dce872dcb889ac81a29ec3a116be1a1
vinaypuppal <me@vinaypuppal.com>
2020-04-20 13:30:34 +0530

    fix peer dependencies import

--- src/middlewares/static.ts
+++ src/middlewares/static.ts
@@ -1,4 +1,3 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 import { Options } from "@cloudflare/kv-asset-handler/src/types";
 
 import { EdgeRequest } from "../request";
@@ -6,6 +5,7 @@ import { EdgeResponse } from "../response";
 import { RouteHandler } from "../types";
 
 export function workersSite(options?: Partial<Options>): RouteHandler {
+  const { getAssetFromKV } = require("@cloudflare/kv-asset-handler");
   return async (req: EdgeRequest, _res: EdgeResponse) => {
     try {
       const response = await getAssetFromKV(req._event, options);
